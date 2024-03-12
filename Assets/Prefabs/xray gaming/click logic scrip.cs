be3142c5e263d4bb20387b2083d614635d41d334
aaron-stansfield <aaronmstansfield@gmail.com>
2024-03-12 11:28:30 +0000

    xray good complete

--- Assets/Prefabs/xray gaming/click logic scrip.cs
+++ Assets/Prefabs/xray gaming/click logic scrip.cs
@@ -7,6 +7,9 @@ using TMPro;
 public class clicklogicscrip : MonoBehaviour
 {
     public yummyscrip bam;
+    public GameObject blam;
+    public Sprite yes;
+    public Sprite no;
 
 
     //please ignore how i did this im lazy
@@ -16,11 +19,14 @@ public class clicklogicscrip : MonoBehaviour
         if (slap == 0)
         {
             print("win");
+            blam.GetComponent<Image>().sprite = yes;
         }
         else
         {
             print("lose");
+            blam.GetComponent<Image>().sprite = no;
         }
+        blam.SetActive(true);
 
     }
 
@@ -30,12 +36,14 @@ public class clicklogicscrip : MonoBehaviour
         if (slap == 1)
         {
             print("win");
+            blam.GetComponent<Image>().sprite = yes;
         }
         else
         {
             print("lose");
+            blam.GetComponent<Image>().sprite = no;
         }
-
+        blam.SetActive(true);
     }
     public void on_click_purple()
     {
@@ -43,26 +51,30 @@ public class clicklogicscrip : MonoBehaviour
         if (slap == 2)
         {
             print("win");
+            blam.GetComponent<Image>().sprite = yes;
         }
         else
         {
             print("lose");
+            blam.GetComponent<Image>().sprite = no;
         }
-
+        blam.SetActive(true);
     }
 
-    public void on_click_cyan()
+    public void on_click_orange()
     {
         int slap = bam.rando;
         if (slap == 3)
         {
             print("win");
+            blam.GetComponent<Image>().sprite = yes;
         }
         else
         {
             print("lose");
+            blam.GetComponent<Image>().sprite = no;
         }
-
+        blam.SetActive(true);
     }
     public void on_click_yellow()
     {
@@ -70,12 +82,14 @@ public class clicklogicscrip : MonoBehaviour
         if (slap == 4)
         {
             print("win");
+            blam.GetComponent<Image>().sprite = yes;
         }
         else
         {
             print("lose");
+            blam.GetComponent<Image>().sprite = no;
         }
-
+        blam.SetActive(true);
     }
     public void on_click_green()
     {
@@ -83,11 +97,13 @@ public class clicklogicscrip : MonoBehaviour
         if (slap == 5)
         {
             print("win");
+            blam.GetComponent<Image>().sprite = yes;
         }
         else
         {
             print("lose");
+            blam.GetComponent<Image>().sprite = no;
         }
-
+        blam.SetActive(true);
     }
 }
