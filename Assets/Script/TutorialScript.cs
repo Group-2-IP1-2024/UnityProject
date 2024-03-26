031dfbd836a92f74ad5266ecdcc317219e420b43
Ross McRobie <42011113+TheProgrammingDucky@users.noreply.github.com>
2024-03-26 13:57:03 +0000

    Tutorial TextBox and Options menu
    Updated tutorial textbox and improved the options menu considerably

--- Assets/Script/TutorialScript.cs
+++ Assets/Script/TutorialScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TutorialScript : MonoBehaviour
 {
@@ -19,6 +20,7 @@ public class TutorialScript : MonoBehaviour
 
     public void Endtut()
     {
+        SceneManager.LoadScene("Main Game");
         Time.timeScale = 1f;
     }
 
